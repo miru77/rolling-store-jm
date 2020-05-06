5b584171ad78fa3735cabe44e81353c0a0c445f8
jorge miranda <jorgemiranda1977@gmail.com>
2020-05-06 16:30:30 -0300

    primer comit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprender React
         </a>
       </header>
     </div>
